e6ddc795cdc4ba57e5e0a41f50ab06a4e4479544
Zazzie Wharton <zazziewharton@gmail.com>
2026-06-01 18:59:45 +0100

    hoping this works

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function onMouseMove(event) {
     var path = new Path.Circle({
         center: event.middlePoint,
-        radius: 10
+        radius: Math.round(Math.random() * 25) + 5
     })
     path.fillColor = {
-        hue: 0,
+        hue: Math.random() * 360,
         saturation: 1,
         brightness: 1
     }
